66fdf0bd357a1ff0ef91638b5d33f265a57a6667
dlstar89 <dlstarcenko@gmail.com>
2018-12-15 14:59:59 +0000

    TransBookDao
    Bug Fixes

--- src/mainlibrary/TransBookDao.java
+++ src/mainlibrary/TransBookDao.java
@@ -1,142 +1,183 @@
 package mainlibrary;
 
-import java.sql.*;
-import javax.swing.JTextField;
+import liblogger.LibLogger;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class TransBookDao {
+    private TransBookDao() { }
 
     public static boolean checkBook(String bookcallno) {
         boolean status = false;
-        try {
-            Connection con = DB.getConnection();
-            PreparedStatement ps = con.prepareStatement("select * from Books where BookID=?");
-            ps.setString(1, bookcallno);
-            ResultSet rs = ps.executeQuery();
-            status = rs.next();
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        try (Connection con = DB.getConnection()) {
+            try (PreparedStatement ps = con.prepareStatement("select * from Books where BookID=?")) {
+                ps.setString(1, bookcallno);
+                try (ResultSet rs = ps.executeQuery()) {
+                    status = rs.next();
+                } catch (SQLException e) {
+                    LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+                }
+            } catch (SQLException e) {
+                LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+            }
+        } catch (IOException | SQLException e) {
+            LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
         }
+
         return status;
     }
 
     public static boolean BookValidate(String BookID) {
         boolean status = false;
         try (Connection con = DB.getConnection()) {
-            PreparedStatement ps = con.prepareStatement("select * from Books where BookID = ?");
-            ps.setString(1, BookID);
-            ResultSet rs = ps.executeQuery();
-            status = rs.next();
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+            try (PreparedStatement ps = con.prepareStatement("select * from Books where BookID = ?")) {
+                ps.setString(1, BookID);
+                try (ResultSet rs = ps.executeQuery()) {
+                    status = rs.next();
+                } catch (SQLException e) {
+                    LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+                }
+            } catch (SQLException e) {
+                LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+            }
+        } catch (IOException | SQLException e) {
+            LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
         }
+
         return status;
     }
 
     public static boolean UserValidate(String UserID) {
         boolean status = false;
         try (Connection con = DB.getConnection()) {
-            PreparedStatement ps = con.prepareStatement("select * from Users where UserID = ?");
-            ps.setString(1, UserID);
-            ResultSet rs = ps.executeQuery();
-            status = rs.next();
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+            try (PreparedStatement ps = con.prepareStatement("select * from Users where UserID = ?")) {
+                ps.setString(1, UserID);
+                try (ResultSet rs = ps.executeQuery()) {
+                    status = rs.next();
+                } catch (SQLException e) {
+                    LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+                }
+            } catch (SQLException e) {
+                LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+            }
+        } catch (IOException | SQLException e) {
+            LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
         }
+
         return status;
     }
 
     public static int updatebook(String bookcallno) {
         int status = 0;
-        int quantity = 0, issued = 0;
-        try {
-            Connection con = DB.getConnection();
+        int quantity = 0;
+        int issued = 0;
+        try (Connection con = DB.getConnection()) {
+            try (PreparedStatement ps = con.prepareStatement("select quantity,issued from books where callno=?")) {
+                ps.setString(1, bookcallno);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        quantity = rs.getInt("quantity");
+                        issued = rs.getInt("issued");
+                        if (quantity > 0) {
+                            try (PreparedStatement ps2 = con.prepareStatement("update books set quantity=?,issued=? where callno=?")) {
+                                ps2.setInt(1, quantity - 1);
+                                ps2.setInt(2, issued + 1);
+                                ps2.setString(3, bookcallno);
 
-            PreparedStatement ps = con.prepareStatement("select quantity,issued from books where callno=?");
-            ps.setString(1, bookcallno);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                quantity = rs.getInt("quantity");
-                issued = rs.getInt("issued");
+                                status = ps2.executeUpdate();
+                            } catch (SQLException e) {
+                                LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+                            }
+                        }
+                    }
+                } catch (SQLException e) {
+                    LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+                }
+            } catch (SQLException e) {
+                LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
             }
-
-            if (quantity > 0) {
-                PreparedStatement ps2 = con.prepareStatement("update books set quantity=?,issued=? where callno=?");
-                ps2.setInt(1, quantity - 1);
-                ps2.setInt(2, issued + 1);
-                ps2.setString(3, bookcallno);
-
-                status = ps2.executeUpdate();
-            }
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (IOException | SQLException e) {
+            LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
         }
         return status;
     }
 
     public static int IssueBook(int BookID, int UserID, String IDate, String RDate) {
         int status = 0;
-        try {
-
-            Connection con = DB.getConnection();
-            PreparedStatement ps = con.prepareStatement("insert into IssuedBook values(?,?,?,?)");
-            ps.setInt(1, BookID);
-            ps.setInt(2, UserID);
-            ps.setString(3, IDate);
-            ps.setString(4, RDate);
-            status = ps.executeUpdate();
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        try (Connection con = DB.getConnection()) {
+            try (PreparedStatement ps = con.prepareStatement("insert into IssuedBook values(?,?,?,?)")) {
+                ps.setInt(1, BookID);
+                ps.setInt(2, UserID);
+                ps.setString(3, IDate);
+                ps.setString(4, RDate);
+                status = ps.executeUpdate();
+            } catch (SQLException e) {
+                LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+            }
+        } catch (IOException | SQLException e) {
+            LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
         }
+
         return status;
     }
 
     public static int ReturnBook(int BookID, int UserID) {
         int status = 0;
-        try {
-
-            Connection con = DB.getConnection();
-            PreparedStatement ps = con.prepareStatement("delete from IssuedBook where BookID=? and UserID=?");
-            ps.setInt(1, BookID);
-            ps.setInt(2, UserID);
-            status = ps.executeUpdate();
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        try (Connection con = DB.getConnection()) {
+            try (PreparedStatement ps = con.prepareStatement("delete from IssuedBook where BookID=? and UserID=?")) {
+                ps.setInt(1, BookID);
+                ps.setInt(2, UserID);
+                status = ps.executeUpdate();
+            } catch (SQLException e) {
+                LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+            }
+        } catch (IOException | SQLException e) {
+            LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
         }
+
         return status;
     }
 
     public static boolean CheckIssuedBook(int BookID) {
         boolean status = false;
         try (Connection con = DB.getConnection()) {
-            PreparedStatement ps = con.prepareStatement("select * from IssuedBook  where BookID=?");
-            ps.setInt(1, BookID);
-            ResultSet rs = ps.executeQuery();
-            status = rs.next();
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+            try (PreparedStatement ps = con.prepareStatement("select * from IssuedBook  where BookID=?")) {
+                ps.setInt(1, BookID);
+                try (ResultSet rs = ps.executeQuery()) {
+                    status = rs.next();
+                } catch (SQLException e) {
+                    LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+                }
+            } catch (SQLException e) {
+                LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+            }
+        } catch (IOException | SQLException e) {
+            LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
         }
+
         return status;
     }
 
     public static int Check(int UserID) {
-        boolean status = false;
         int num = 0;
         try (Connection con = DB.getConnection()) {
-            PreparedStatement ps = con.prepareStatement("select * from Book_Count UserID=?");
-            ps.setInt(2, UserID);
-            ResultSet rs = ps.executeQuery();
-            status = rs.next();
-            num = rs.getInt("BookNo");
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+            try (PreparedStatement ps = con.prepareStatement("select * from Book_Count UserID=?")) {
+                ps.setInt(1, UserID);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next())
+                        num = rs.getInt("BookNo");
+                } catch (SQLException e) {
+                    LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+                }
+            } catch (SQLException e) {
+                LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
+            }
+        } catch (IOException | SQLException e) {
+            LibLogger.logMessageSEVERE(TransBookDao.class, e.toString());
         }
         if (num == 5) {
             return 0;
