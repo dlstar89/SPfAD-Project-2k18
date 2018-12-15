f2660a0dbd7bde3fc7922b17cfeac17cfe140abd
dlstar89 <dlstarcenko@gmail.com>
2018-12-15 01:55:13 +0000

    LibrarianDao
    Bug Fixes

--- src/mainlibrary/LibrarianDao.java
+++ src/mainlibrary/LibrarianDao.java
@@ -1,57 +1,68 @@
 package mainlibrary;
 
+import liblogger.LibLogger;
+
+import java.io.IOException;
 import java.sql.*;
 
 public class LibrarianDao {
+    private LibrarianDao() { }
 
     public static int save(String name, String password, String email, String address, String city, String contact) {
         int status = 0;
-        try {
-
-            Connection con = DB.getConnection();
-            PreparedStatement ps = con.prepareStatement("insert into librarian(name,password,email,address,city,contact) values(?,?,?,?,?,?)");
-            ps.setString(1, name);
-            ps.setString(2, password);
-            ps.setString(3, email);
-            ps.setString(4, address);
-            ps.setString(5, city);
-            ps.setString(6, contact);
-            status = ps.executeUpdate();
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        try (Connection con = DB.getConnection()) {
+            try (PreparedStatement ps = con.prepareStatement("insert into librarian(name,password,email,address,city,contact) values(?,?,?,?,?,?)")) {
+                ps.setString(1, name);
+                ps.setString(2, password);
+                ps.setString(3, email);
+                ps.setString(4, address);
+                ps.setString(5, city);
+                ps.setString(6, contact);
+                status = ps.executeUpdate();
+            } catch (SQLException e) {
+                LibLogger.logMessageSEVERE(LibrarianDao.class, e.toString());
+            }
+        } catch (IOException | SQLException e) {
+            LibLogger.logMessageSEVERE(LibrarianDao.class, e.toString());
         }
+
         return status;
     }
 
     public static int delete(int id) {
         int status = 0;
-        try {
-            Connection con = DB.getConnection();
-            PreparedStatement ps = con.prepareStatement("delete from Librarian where id=?");
-            ps.setInt(1, id);
-            status = ps.executeUpdate();
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        try (Connection con = DB.getConnection()) {
+            try (PreparedStatement ps = con.prepareStatement("delete from Librarian where id=?")) {
+                ps.setInt(1, id);
+                status = ps.executeUpdate();
+            } catch (SQLException e) {
+                LibLogger.logMessageSEVERE(LibrarianDao.class, e.toString());
+            }
+        } catch (IOException | SQLException e) {
+            LibLogger.logMessageSEVERE(LibrarianDao.class, e.toString());
         }
+
         return status;
     }
 
     public static boolean validate(String name, String password) {
         boolean status = false;
-        try {
-            Connection con = DB.getConnection();
-            String select = "select * from Librarian where UserName= '" + name + "' and Password='"+ password +"'";
-            Statement selectStatement = con.createStatement();
-            ResultSet rs = selectStatement.executeQuery(select);
-          
-            status = rs.next();
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        try (Connection con = DB.getConnection()) {
+            try (PreparedStatement ps = con.prepareStatement("select * from Librarian where UserName=? and Password=?")) {
+                ps.setString(1, name);
+                ps.setString(2, password);
+                try (ResultSet rs = ps.executeQuery()) {
+                    status = rs.next();
+                } catch (SQLException e) {
+                    LibLogger.logMessageSEVERE(LibrarianDao.class, e.toString());
+                }
+            } catch (SQLException e) {
+                LibLogger.logMessageSEVERE(LibrarianDao.class, e.toString());
+            }
+        } catch (IOException | SQLException e) {
+            LibLogger.logMessageSEVERE(LibrarianDao.class, e.toString());
         }
+
         return status;
     }
-
 }
